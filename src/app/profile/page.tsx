207314b964f2b0865deddbaff56f276ff36c081c
Lukman Hakim <madlukmanhakim@gmail.com>
2024-03-19 21:53:58 +0700

    Jawaban Praktikum 1

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <>
+        <h1>Muhammad Lukman Hakim / 2141720109</h1> 
+        <h2>Mahasiswa</h2>
+        <h4>Hamba Allah yang sabar :)</h4>
+        </>
+        
+    )
+  }
